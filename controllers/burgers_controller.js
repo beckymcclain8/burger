6ac795c54f9fb675baba1a6977e8d67fd321fb69
Becky McClain <beckykelley8@yahoo.com>
2018-05-09 07:27:27 -0700

    updateOne still not working.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,10 +2,6 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
-// router.get("/", function(req, res) {
-//   res.redirect("/burgers");
-// });
-
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {
@@ -16,54 +12,22 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
-  burger.insertOne([
-    "burger_name", "devoured"
-  ], [
-    req.body.burger_name,
-    false
-  ], function() {
+router.post("/burgers/create", function(req, res) {
+  burger.insertOne(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, false],
+    function() {
+      res.redirect("/");
+    }
+  );
+});
+
+router.put("/burgers/update", function(req, res) {
+  // var condition = "id = " + req.body.id;
+  burger.updateOne(req.body.devoured, condition, function(result) {
+    console.log(result);
     res.redirect("/");
-    // res.json({id});
   });
 });
 
-
-
-
-
-
-
-
-
-// router.get("/", function(req, res) {
-//   burger.selectAll(function(data) {
-//     var hbsObject = {
-//       burgers: data
-//     };
-//     console.log(hbsObject);
-//     res.render("index", hbsObject);
-//   });
-// });
-
-// router.post("/api/burgers", function(req, res) {
-//   burger.insertOne([
-//     "burger_name"
-//   ], [
-//     req.body.burger_name
-//   ], function(result) {
-//     // Send back the ID of the new quote
-//     console.log("req.body.burger_name: " + req.body.burger_name);
-//     res.json({ id: result.insertId });
-//   });
-// });
-
-router.post("/burgers/create", function(req, res) {
-  burger.insertOne(["burger_name"], [req.body.burger_name], function() {
-    console.log(result);
-    res.redirect("/burgers");
-  })
-})
-
-  module.exports = router;
-
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,19 +1,21 @@
-
 var orm = require("../config/orm.js");
 
 var burger = {
-    selectAll: function(cb) {
-        orm.selectAll("burgers", function(res) {
-          cb(res);
-        });
-      },
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
 
-      insertOne: function(cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, cb);
-        //   orm.insertOne("burgers", ["burger_name", "devoured"], [name, false], cb);
-            
-        },
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, cb);
+  },
 
+  updateOne: function(condition, cb) {
+    // var condition = "id=" + id;
+    orm.updateOne("burgers", {devoured: true}, condition, cb);
+  
+  }
 };
 
 module.exports = burger;
